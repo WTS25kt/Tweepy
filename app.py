4e9ad88fe747447eb015b987ca48b06e03db929a
WTS25kt <ishii25kenta@gmail.com>
2024-07-30 15:24:40 +0900

    失敗　dotenvのインストールしていない

---
+++ app.py
@@ -0,0 +1,18 @@
+import tweepy
+import os
+
+# 環境変数からTwitter APIキーを取得
+consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
+consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
+access_token = os.environ["TWITTER_ACCESS_TOKEN"]
+access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+
+# Tweepyの認証設定
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# ツイート内容を生成
+tweet_content = "これは自動投稿されたツイートです。#自動投稿 #Python"
+
+# ツイートを投稿
+api.update_status(status=tweet_content)
