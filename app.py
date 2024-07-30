661ca1320eaf28bf1203981da65f0e021ebe9b28
WTS25kt <ishii25kenta@gmail.com>
2024-07-30 15:37:28 +0900

    成功

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 import tweepy
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 # .envファイルを読み込む
 load_dotenv()
 
 # 環境変数からTwitter APIキーを取得
-consumer_key = os.environ["TWITTER_CONSUMER_KEY"]
-consumer_secret = os.environ["TWITTER_CONSUMER_SECRET"]
-access_token = os.environ["TWITTER_ACCESS_TOKEN"]
-access_token_secret = os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
+consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
+access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
 # Tweepyの認証設定
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
 
 # ツイート内容を生成
 tweet_content = "これは自動投稿されたツイートです。#自動投稿 #Python"
 
 # ツイートを投稿
-api.update_status(status=tweet_content)
+client.create_tweet(text=tweet_content)
